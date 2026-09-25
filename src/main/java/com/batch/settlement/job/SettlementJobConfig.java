712cf9722b99ca8355da3079f29377b9c05ad8d2
baekkayoung <baekkayoung@naver.com>
2026-09-25 23:47:18 +0900

    feat: Step 및 Job 등록

---
+++ src/main/java/com/batch/settlement/job/SettlementJobConfig.java
@@ -0,0 +1,76 @@
+package com.batch.settlement.job;
+
+import com.batch.settlement.domain.Orders;
+import com.batch.settlement.domain.Settlement;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.batch.infrastructure.item.database.JpaItemWriter;
+import org.springframework.batch.infrastructure.item.database.JpaPagingItemReader;
+import org.springframework.batch.infrastructure.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.infrastructure.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class SettlementJobConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Step settlementStep() {
+        return new StepBuilder("settlementStep",jobRepository)
+                .<Orders,Settlement>chunk(1000)
+                .transactionManager(transactionManager)
+                .reader(ordersReader(null))
+                .processor(settlementProcessor())
+                .writer(settlementWriter())
+                .build();
+    }
+
+    //ItemReader
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Orders> ordersReader(@Value("#{jobParameter['targetDate']}")String targetDate) {
+        log.info("[Reader] 정산 집계 대상 날짜 : {}", targetDate);
+
+        return new JpaPagingItemReaderBuilder<Orders>()
+                .name("orderReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(1000)
+                .queryString("SELECT o FROM Orders o WHERE o.orderDate = :targetDate ORDER BY o.id")
+                .parameterValues(Collections.singletonMap("targetDate", LocalDate.parse(targetDate)))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Orders, Settlement> settlementProcessor(){ // <Input, Output>
+        return item -> {
+            int fee = (int) (item.getAmount() * 0.03); // 수수료 3% 계산
+            int settlementAmount = item.getAmount() - fee; // 정산금액 계산
+
+            return new Settlement(item.getId(), item.getStoreName(), settlementAmount, LocalDate.now());
+        };
+    }
+
+    @Bean
+    public JpaItemWriter<Settlement> settlementWriter(){
+        return new JpaItemWriterBuilder<Settlement>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+}
